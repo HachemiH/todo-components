098f361fe3f091ea30e6486a8d8b570129819658
Hachemi <hachemi@protonmail.com>
2019-10-06 17:56:38 +0200

    Etape 10 : Création du composant TodoListItem.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <TodoInputText />
+    <TodoListItem />
   </div>
 </template>
 
 <script>
 import TodoInputText from "@/components/TodoInputText.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
 export default {
   components: {
-    TodoInputText
+    TodoInputText,
+    TodoListItem
   },
   data() {
     return {
