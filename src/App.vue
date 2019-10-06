b9bec5393de43b4d8c1d5089cff6671b70dc5f07
Hachemi <hachemi@protonmail.com>
2019-10-06 16:19:37 +0200

    Etape 05 : cabler le composant TodoList.vue  vers App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <h1>Hello World !!!</h1>
+    <TodoList />
   </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList.vue";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    TodoList
+  }
 };
 </script>
 
